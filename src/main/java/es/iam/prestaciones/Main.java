dcdcc701a56381767207b9d63f16c2e3c99ec7a4
acastanonm <acastanonm@minsait.com>
2025-02-14 12:00:43 +0100

    Update Main.java

--- src/main/java/es/iam/prestaciones/Main.java
+++ src/main/java/es/iam/prestaciones/Main.java
@@ -3,7 +3,7 @@ package es.iam.prestaciones;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Prueba GitHub");
+        System.out.println("Prueba GitHub error modificada");
         System.out.println("Prueba Git San Valentin");
     }
 }
